33abaa859dd8bd1f6862bec2eceae0e0757a3578
= <=>
2014-10-27 21:19:26 +0800

    Initial Version

---
+++ douban_seek.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+mps.
+
+https://github.com/xiazcx/douban_seek
+
+Copyright (C)  2014 nagev
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+
+
+__version__ = "0.20.16"
+__author__ = "nagev"
+__license__ = "GPLv3"
+
+import urllib
+import urllib2
+import logging
+import hashlib
+import difflib
+import random
+import socket
+import zlib
+import time
+import math
+import json
+import sys
+import re
+import os
+
+
+def main():
+	print "Start to seek your friends!"
+
+main()
+
